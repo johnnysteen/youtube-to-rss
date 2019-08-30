0e61d7ee742790b5109047b2ddfe24901ece9a64
johnnysteen <52551335+johnnysteen@users.noreply.github.com>
2019-08-30 14:19:18 -0400

    Add files via upload

--- run-updates.sh
+++ run-updates.sh
@@ -1,24 +1,37 @@
 #!/bin/bash
 
-documentroot=/Library/WebServer/Documents/ #Whatever directory has this file in it
-feeds=( rekieta examplefeed examplefeed2 ) #Edit this with list of directories with feeds you want
+documentroot=/Library/WebServer/Documents/
+#feeds=( rekieta aydin metokur josh soph gator )
 
-uptime=6 #Hour (in 24 hour clock) at which you want to update
+uptime=8
 
 while :
 do
     echo 'The time is ' `date +%H:%M:%S`
-    if [ `date +%H` -eq $uptime ]
-    then
+    hour=`date +%H`
+    hour=${hour#0}
+    #if [ `date +%H` -eq $uptime ]
+    if ! (( hour % uptime )); then
+        youtube-dl -U
         echo 'Updating feeds...'
-        for feed in "${feeds[@]}"
+        while read LINE
         do
-            cd $documentroot/$feed
+            echo $LINE
+            cd $documentroot/$LINE
             bash -x ./update.sh
-        done
+            cd $documentroot
+        done < feeds
+        #for feed in "${feeds[@]}"
+        #do
+        #    cd $documentroot/$feed
+        #    bash -x ./update.sh
+        #    cd $documentroot
+        #done
     else
         echo 'Will update at' $uptime:`date +%M:%S`
     fi
     echo 'Will return in one hour.'
     sleep 3600
 done
+
+
--- update.sh
+++ update.sh
@@ -13,15 +13,16 @@ grep -i '\[download\] destination' log.out > log2.out
 
 while read LINE
 do
-    oldfile=${LINE#*: }                 #Long F@$&* title!-1234567890A.webm
-    filebase=${oldfile%.*}              #Long F@$&* title!-1234567890A
+    oldfile=${LINE#*: }                 #Long Fucking title!-1234567890A.webm
+    filebase=${oldfile%.*}              #Long Fucking title!-1234567890A
     list_of_files=( "$filebase".* )
-    oldfile="${list_of_files[0]}"       #Long F@$&* title!-1234567890A.mp4
+    oldfile="${list_of_files[0]}"       #Long Fucking title!-1234567890A.mp4
     fileext=${oldfile##*.}              #mp4
     newfile=${filebase:(-11)}.$fileext  #1234567890A.mp4
     length=`ffmpeg -i "$oldfile" 2>&1 | grep Duration | awk -F: '{print 3600 * $2 + 60*$3 + $4 }'`
     mv "$oldfile" $newfile
     mv feed.body feed.oldbody
+    oldfile=$(echo $oldfile | sed 's/\&/\&amp\;/g;s/\</\&lt\;/g;s/\>/\&gt\;/g')
     cat << EOF >> feed.body
     <item>
     <title>${oldfile%-*}</title>
