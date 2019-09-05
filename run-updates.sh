8b053040215df633fdeda1771b2c8ae6490ab73f
johnnysteen <52551335+johnnysteen@users.noreply.github.com>
2019-09-05 09:52:25 -0400

    Add files via upload

--- run-updates.sh
+++ run-updates.sh
@@ -1,37 +1,30 @@
 #!/bin/bash
 
 documentroot=/Library/WebServer/Documents/
-#feeds=( rekieta aydin metokur josh soph gator )
 
 uptime=8
+let interval=3600*$uptime
 
 while :
 do
     echo 'The time is ' `date +%H:%M:%S`
-    hour=`date +%H`
-    hour=${hour#0}
-    #if [ `date +%H` -eq $uptime ]
-    if ! (( hour % uptime )); then
-        youtube-dl -U
-        echo 'Updating feeds...'
-        while read LINE
-        do
-            echo $LINE
-            cd $documentroot/$LINE
-            bash -x ./update.sh
-            cd $documentroot
-        done < feeds
-        #for feed in "${feeds[@]}"
-        #do
-        #    cd $documentroot/$feed
-        #    bash -x ./update.sh
-        #    cd $documentroot
-        #done
-    else
-        echo 'Will update at' $uptime:`date +%M:%S`
-    fi
-    echo 'Will return in one hour.'
-    sleep 3600
+
+    youtube-dl -U
+    echo 'Updating feeds...'
+    while read LINE
+    do
+        echo $LINE
+        cd $documentroot/$LINE
+        bash -x ./update.sh
+        cd $documentroot
+    done < feeds
+
+    hr=$(expr $(date +%H) % $uptime)
+    let mins=$(date +%M)+60*$hr
+    let secs=$(date +%S)+60*$mins
+    let sleeptime=$interval-$secs
+    echo 'Will return in ' $sleeptime ' seconds.'
+    sleep $sleeptime
 done
 
 
