e6faf61082025008b1eda3011cdc26899ebbf1fe
johnnysteen <johnawbcostanzo@gmail.com>
2020-03-23 11:54:52 -0500

    Now takes max downloads as command line arg

--- update.sh
+++ update.sh
@@ -6,7 +6,7 @@ cd $documentroot/$1
 . params
 
 
-youtube-dl --write-info-json -xciw --max-downloads 1 --download-archive archive $channelurl > log.out
+youtube-dl --write-info-json -xciw --max-downloads $2 --download-archive archive $channelurl > log.out
 
 grep -i '\[download\] destination' log.out > log2.out
 
@@ -14,18 +14,23 @@ while read LINE
 do
     oldfile=${LINE#*: }                 #Long F@#$ title!-1234567890A.webm
     filebase=${oldfile%.*}              #Long F@#$ title!-1234567890A
+
     echo 'New episode: ' $filebase
 
     pubdate=`cat "$filebase.info.json" | python3 -c "import sys, json; print(json.load(sys.stdin)['upload_date'])"`
+    pubstring=`date -jf '%Y%m%d' '+%a, %d %b %Y %H:%M:%S %z' $pubdate`
     description=`cat "$filebase.info.json" | python3 -c "import sys, json; print(json.load(sys.stdin)['description'])" | sed 's/\&/\&amp\;/g;s/\</\&lt\;/g;s/\>/\&gt\;/g'`
 
+    echo 'Published ' "$pubstring"
 
     rm -- "$filebase.info.json"
+
     list_of_files=( "$filebase".* )
     oldfile="${list_of_files[0]}"       #Long F@#$ title!-1234567890A.mp4
     fileext=${oldfile##*.}              #mp4
     newfile=${filebase:(-11)}.$fileext  #1234567890A.mp4
     length=`ffmpeg -i "$oldfile" 2>&1 | grep Duration | awk -F: '{print 3600 * $2 + 60*$3 + $4 }'`
+
     mv "$oldfile" $newfile
     oldfile=$(echo $oldfile | sed 's/\&/\&amp\;/g;s/\</\&lt\;/g;s/\>/\&gt\;/g')
 
@@ -34,7 +39,7 @@ do
     cat << EOF >> feed.body
     <item>
     <title>${oldfile%-$newfile}</title>
-    <pubdate>`date -jf '%Y%m%d' '+%a, %d %b %Y %H:%M:%S %z' $pubdate`</pubdate>
+    <pubdate>$pubstring</pubdate>
     <enclosure url="http://$URL/$feedname/$newfile" type="audio/mpeg" length="$length"></enclosure>
     <description>
     $description
@@ -53,7 +58,7 @@ cat << EOF > feed.newrss
     <title>$feedtitle</title>
     <link>https://$URL/$feedname/</link>
     <description>$feedtitle Youtube rips</description>
-    <image>https://$URL/$feedname/favicon.jpg</image>
+    <image>$thumburl</image>
     <generator>Visit johnnysteen/youtube-to-rss on Github</generator>
 EOF
 
