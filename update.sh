360a63becc30af6b5f3f098c7ad2224c11d9996a
johnnysteen <johnawbcostanzo@gmail.com>
2021-12-20 09:18:46 -0600

    add some features to make threadsafe

--- update.sh
+++ update.sh
@@ -2,13 +2,15 @@
 echo '**********' `date` '**********' 1>&2
 echo $PATH 1>&2
 PATH=$PATH:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin
-cd /Users/jcost/youtube-to-rss/
+ytdlpath=/home/pi/youtube-to-rss
+cd $ytdlpath
 
 . config
 
 cd $documentroot/$1
 . params
 
+overridelock=0
 OPTIND=2
 while getopts ":n:v:" arg; do
     case $arg in
@@ -17,37 +19,49 @@ while getopts ":n:v:" arg; do
             ;;
         v)
             channelurl=${OPTARG}
+            overridelock=1
             ;;
     esac
 done
 
+if [ "$overridelock" -eq 0 ]; then
+    if [ -f LOCK ]; then
+        echo "Update already in progress."
+        exit 0
+fi
+fi
+
+touch LOCK
 #if [ -n "$2" ] && [ "$2" -eq "$2" ] 2>/dev/null; then
 #    numdl=$2
 #else
 #    numdl=1
 #fi
 
+uniqid=$(date '+%s')
+
 #youtube-dl --datebefore `date -v-1d +%Y%m%d` --restrict-filenames --write-info-json -xciw --yes-playlist --max-downloads $numdl --download-archive archive $channelurl > log.out
-youtube-dl --cookies /Users/jcost/youtube-to-rss/youtube.com_cookies.txt --id --abort-on-unavailable-fragment --restrict-filenames --write-info-json -xci --yes-playlist --max-downloads $numdl --download-archive archive $channelurl > log.out
+youtube-dl --cookies $ytdlpath/youtube.com_cookies.txt -o "%(id)s${uniqid}.%(ext)s" --abort-on-unavailable-fragment --restrict-filenames --write-info-json -xci --yes-playlist --max-downloads $numdl --download-archive archive $channelurl > log.out
 
 grep -i '\[download\] destination' log.out > log2.out
 
-for finfojson in *.info.json;
+for finfojson in *${uniqid}.info.json;
 do
 if [ -f $finfojson ]; then
     filebase=${finfojson%%.*}
 
 
-    title=`cat "$finfojson" | python -c "import sys, json; print(json.load(sys.stdin)['title'])" | sed 's/\&/\&amp\;/g;s/\</\&lt\;/g;s/\>/\&gt\;/g'`
-    vidurl=`cat "$finfojson" | python -c "import sys, json; print(json.load(sys.stdin)['webpage_url'])"`
-    pubdate=`cat "$finfojson" | python -c "import sys, json; print(json.load(sys.stdin)['upload_date'])"`
-    pubstring=`date -jf '%Y%m%d' '+%a, %d %b %Y %H:%M:%S %z' $pubdate`
-    description=`cat "$finfojson" | python -c "import sys, json; print(json.load(sys.stdin)['description'].encode('utf-8'))" | sed 's/\&/\&amp\;/g;s/\</\&lt\;/g;s/\>/\&gt\;/g'`
+    title=`cat -- "${finfojson}" | python -c "import sys, json; print(json.load(sys.stdin)['title'])" | sed 's/\&/\&amp\;/g;s/</\&lt\;/g;s/>/\&gt\;/g'`
+    vidurl=`cat -- "${finfojson}" | python -c "import sys, json; print(json.load(sys.stdin)['webpage_url'])"`
+    pubdate=`cat -- "${finfojson}" | python -c "import sys, json; print(json.load(sys.stdin)['upload_date'])"`
+    pubstring=`date --date $pubdate '+%a, %d %b %Y %H:%M:%S %z'`
+    description=`cat -- "${finfojson}" | python -c "import sys, json; print(json.load(sys.stdin)['description'].encode('utf-8'))" | sed 's/\&/\&amp\;/g;s/</\&lt\;/g;s/>/\&gt\;/g'`
+    vid=`cat -- "${finfojson}" | python -c "import sys,json; print(json.load(sys.stdin)['id'])"`
 
     echo 'New episode: ' $title
     echo 'Published ' "$pubstring"
 
-    rm -- "$finfojson"
+    rm -- "${finfojson}"
 
     list_of_files=( "$filebase".* )
     oldfile="${list_of_files[0]}"
@@ -60,11 +74,12 @@ if [ -f $finfojson ]; then
     <item>
     <title>${title}</title>
     <pubdate>$pubstring</pubdate>
-    <guid>${filebase}</guid>
+    <guid>${vid}</guid>
     <enclosure url="http://$URL/$feedname/$newfile" type="audio/mpeg" length="$length"></enclosure>
     <description>
     ${title}
 
+    Filename: $newfile
     Original video: $vidurl
     Downloaded: `date`
 
@@ -99,4 +114,5 @@ EOF
 
 rm log2.out
 mv feed.newrss feed.rss
+rm LOCK
 
